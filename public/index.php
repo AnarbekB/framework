1ddc09b20b7e00e4615f81ecc57c2752d71fd9e7
Anarbek <AnarbekB@yandex.ru>
2018-11-05 22:45:07 +0300

    add Kernel class

--- public/index.php
+++ public/index.php
@@ -2,33 +2,22 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use App\Kernel;
 
 $request = Request::createFromGlobals();
 
 $routes = include __DIR__.'/../src/routes.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new ControllerResolver();
+$controllerResolver = new ControllerResolver();
 $argumentResolver = new ArgumentResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $resolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (Routing\Exception\RouteNotFoundException $e) {
-    $response = new Response('Not Fount', 404);
-} catch (Exception $e) {
-    $response = new Response($e->getMessage(), 500);
-}
+$kernel = new Kernel($matcher, $controllerResolver, $argumentResolver);
+$response = $kernel->handle($request);
 
 $response->send();
